677c066c10f3a4a88812c9c68b786f8d98030de2
Wera Barinova <alisawera@yandex.ru>
2025-04-23 14:40:27 +0300

    Таблица умножения заданного размера

---
+++ Lesson01/Task3/program.js
@@ -0,0 +1,20 @@
+function multiplication_table() {
+    let table = document.createElement('table');
+    let tr;
+    let td;
+    let textNode;
+    for (let i = 1; i < tbl_size.valueAsNumber; i++) {
+      // Строка таблицы
+      tr = document.createElement('tr');
+      for (let j = 1; j < tbl_size.valueAsNumber + 1; j++) {
+          // Ячейка таблицы
+          td = document.createElement('td');
+          // Текст очередного произведения двух чисел
+          textNode = document.createTextNode('' + i * j);
+          td.appendChild(textNode);
+          tr.appendChild(td);
+      }
+      table.appendChild(tr);
+    }
+    document.body.appendChild(table);
+}
